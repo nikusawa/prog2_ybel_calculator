fe7c8aeace3601eadef1e0431e453a4c2ad74211
Nikita <nikitatows@proton.me>
2026-05-17 18:10:01 +0200

    refactor

--- app/src/main/java/calculator/Calculator.java
+++ app/src/main/java/calculator/Calculator.java
@@ -46,7 +46,7 @@ public class Calculator extends JFrame {
         // TODO
         // Add a new operation "Sub" for the subtraction of two integers as an (instance of a) Java
         // class (you have yet to write this class)
-        operationSelector.addItem(new Sub());
+        operations.put("Sub", new Sub());
 
         // TODO
         // Add a new operation "Mul" for the multiplication of two integers as an anonymous class
--- app/src/main/java/calculator/Sub.java
+++ app/src/main/java/calculator/Sub.java
@@ -1,8 +1,9 @@
 package calculator;
 
 public class Sub implements Operation {
+
     @Override
-    public int apply(int a, int b) {
-        return a - b;
+    public int doOperation(int a, int b) {
+        return a-b;
     }
 }
